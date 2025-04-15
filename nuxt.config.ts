0be2ec3a57e1f40ea7b26a4a6ea3d0ad725fc21e
Marcelo Wiebbelling <mrodrigow@gmail.com>
2025-04-15 10:52:07 +0200

    Nuxt + TailWindCSS

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+    compatibilityDate: '2024-11-01',
+    css: ['~/assets/css/main.css'],
+    devtools: {enabled: true},
+    vite: {
+        plugins: [
+            tailwindcss(),
+        ],
+    },
+})
