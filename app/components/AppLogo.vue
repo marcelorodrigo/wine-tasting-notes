137173a7b21e0761f3e333cd024705b7f1b75f92
Marcelo Wiebbelling <mrodrigow@gmail.com>
2026-03-29 17:06:02 +0200

    chore: clean master branch (remove all files)

--- app/components/AppLogo.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="flex items-center gap-2">
-    <UIcon
-      name="i-lucide-wine"
-      class="text-primary size-6 shrink-0"
-    />
-    <span class="font-display text-lg font-semibold tracking-tight">Wine Tasting Notes</span>
-  </div>
-</template>
